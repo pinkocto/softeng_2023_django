7cd62bc231b6bb6c3b66c5570719e8482b43405f
pinkocto <pucca4881@gmail.com>
2023-11-08 10:17:40 +0900

    미디어 파일 관리하기

--- softeng_2023_prj/settings.py
+++ softeng_2023_prj/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -112,7 +113,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+# 이미지 폴더 지정
 STATIC_URL = "static/"
+
+# 이부분에 추가
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
